26ebc9be20f26926c1563c82ea20ab3a71587fc1
Michi <mbaueretz@googlemail.com>
2015-11-11 20:46:40 +0100

    Löschen von Teams implementiert

--- interface.js
+++ interface.js
@@ -9,14 +9,22 @@ var addTeam = function ()
     $("#new-team").val("")
 }
 
+var deleteTeam = function (event)
+{
+    var team = parseInt($(event.target).attr("data-team"))
+    teams.splice(team, 1)
+    updateTeamList()
+}
+
 var updateTeamList = function ()
 {
     $("#teams").empty()
     for (var i = 0; i < teams.length; i++)
     {
         var team = teams[i]
-        $("<tr><td>" + team + "</td><td><input type=\"button\" value=\"Löschen\" /></td></tr>").appendTo("#teams")
+        $("<tr><td>" + team + "</td><td><input data-team=\"" + i + "\" class=\"delete-team\" type=\"button\" value=\"Löschen\" /></td></tr>").appendTo("#teams")
     }
+    $(".delete-team").click(deleteTeam)
 }
 
 $(document).ready
